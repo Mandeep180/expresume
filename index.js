abcdf15fa421666b02957f7b490c01a71dd82cb3
Mandeep Kaur <mkaur864@my.centennialcollege.ca>
2023-03-24 12:03:06 -0400

    added all the pages

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact.ejs', {title: 'contact'})
   });
+  app.get('/kanban', (req, res) => {
+    res.render('kanban.ejs', {title: 'kanban'})
+  });
+  app.get('/resume', (req, res) => {
+    res.render('resume.ejs', {title: 'resume'})
+  });
+  app.get('/worklog', (req, res) => {
+    res.render('worklog.ejs', {title: 'worklog'})
+  });
+  app.get('/dashboard', (req, res) => {
+    res.render('dashboard.ejs', {title: 'dashboard'})
+  });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,21 @@ app.set('view engine', 'ejs');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
 app.get('/contact', (req, res) => {
   res.render('Contact Page')
 });
-
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard Page')
+});
+app.get('/kanban', (req, res) => {
+  res.render('kanban Page')
+});
+app.get('/worklog', (req, res) => {
+  res.render('worklog Page')
+});
+app.get('/resume', (req, res) => {
+  res.render('resume Page')
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
